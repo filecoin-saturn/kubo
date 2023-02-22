5814c50856913e554a7db0be9679352bd145bf28
Eric Guan <guanzo91@gmail.com>
2023-02-22 13:58:04 -0800

    only test Saturn's trustless gateway

--- test/sharness/lib/test-saturn-L1.sh
+++ test/sharness/lib/test-saturn-L1.sh
@@ -3,15 +3,8 @@
 cd "$(dirname "$0")"
 cd ..
 
-# Skip t0111-gateway-writeable.sh, L1 doesn't support writeable gateway.
-# Skip t0114-gateway-subdomains.sh, L1 doesn't support subdomains.
+# L1s only officially implement the TRUSTLESS_GATEWAY spec.
+# https://github.com/ipfs/specs/blob/main/http-gateways/TRUSTLESS_GATEWAY.md
 
-for file in $(find .  -maxdepth 1 -name 't011*-gateway*.sh' -type f); do
-  echo
-  if [[ $file == *"t0111"* || $file == *"t0114"* ]]; then
-    echo Skip $file
-    continue
-  fi
-  echo Running $file
-  bash $file || exit 1;
-done
+bash t0117-gateway-block.sh || exit 1;
+bash t0118-gateway-car.sh || exit 1;
